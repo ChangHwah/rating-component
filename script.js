857ece352141c3b09de1ee2c101d2e91cfa101cc
Solomon Swager <54381882+ChangHwah@users.noreply.github.com>
2022-08-04 12:43:17 -0400

    Uploaded current work.
    Styling is almost complete for starting page. Need to create functioning script and add "completed" page.

---
+++ script.js
@@ -0,0 +1,33 @@
+// const buttons = document.querySelectorAll('.button')
+// console.log(buttons)
+
+// const activeState = () => {
+//     let active = document.getElementsByClassName('selected')[0];
+//     if (active != undefined) {
+//         active.classList.toggle('selected');
+//         this.classList.toggle('selected');
+//     }
+// }
+
+// buttons.forEach(button => button.addEventListener('click', activeState()))
+
+// const one = document.querySelector('#one')
+// const two = document.querySelector('#two')
+// const three = document.querySelector('#three')
+// const four = document.querySelector('#four')
+// const five = document.querySelector('#five')
+
+// let array = new Array(one,two,three,four,five);
+
+// const func = () => {
+//     for (let i=0 ; i < array.length; i++)
+//     array[i].addEventListener('click', () => {
+//         if (array[i].className == 'selected') {
+//             array[i].remove('selected');
+//         } else {
+//         array[i].classList.add('selected');
+//         }
+
+//     })
+// }
+// func()
