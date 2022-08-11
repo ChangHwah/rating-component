9e28302dd6775ee19d60c40570f1fa80b0fe4a7b
Solomon Swager <zitiswager@gmail.com>
2022-08-10 23:41:59 -0400

    completed project. will add to portfolio

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-// const buttons = document.querySelectorAll('.button')
-// console.log(buttons)
+// Gathering elements to manaipulate
+const submit = document.querySelector('#submit')
+const rating = document.querySelector('.ratingContainer')
+const completeRating = document.querySelector('.completedRating');
+const again = document.querySelector('#again');
+const value = document.getElementById('buttonValue');
+const number = document.querySelectorAll('.numbers');
 
-// const activeState = () => {
-//     let active = document.getElementsByClassName('selected')[0];
-//     if (active != undefined) {
-//         active.classList.toggle('selected');
-//         this.classList.toggle('selected');
-//     }
-// }
+//on submit button click, hide rate start container and show completed rating container
+submit.addEventListener('click', () => {
+  completeRating.classList.toggle('hide');
+  rating.classList.toggle('hide');
+})
 
-// buttons.forEach(button => button.addEventListener('click', activeState()))
+//on rate agaon button click, hide completed rating container and show start rating container
+again.addEventListener('click', () => {
+  rating.classList.toggle('hide');
+  completeRating.classList.toggle('hide');
+})
 
-// const one = document.querySelector('#one')
-// const two = document.querySelector('#two')
-// const three = document.querySelector('#three')
-// const four = document.querySelector('#four')
-// const five = document.querySelector('#five')
-
-// let array = new Array(one,two,three,four,five);
-
-// const func = () => {
-//     for (let i=0 ; i < array.length; i++)
-//     array[i].addEventListener('click', () => {
-//         if (array[i].className == 'selected') {
-//             array[i].remove('selected');
-//         } else {
-//         array[i].classList.add('selected');
-//         }
-
-//     })
-// }
-// func()
+//whenever a button is clicked run a function that will put the html of that button into the text for the completed rating container span.
+number.forEach((numb) => {
+  numb.addEventListener('click', () => {
+    value.innerHTML = numb.innerHTML;
+  })
+})
